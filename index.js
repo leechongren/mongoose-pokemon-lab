f5ce8b1d0ee5c2df7455ac0ba0cd36d306d0ce41
Chong Ren <leechongren@users.noreply.github.com>
2020-02-12 13:45:54 +0800

    changed index.js port to process.env.port

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const PORT = 3000;
 require('./utils/db')
 require("dotenv").config() //read the env file
 
-const server = app.listen(PORT, () => {
+const server = app.listen(process.env.PORT || PORT, () => {
     console.log(`Express app started on http://localhost:${PORT}`);
 });
